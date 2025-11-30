e75dbd549665beb1ba2881b3043be6b2f90cae67
Shafira Khairunisa <125497958+khashafira@users.noreply.github.com>
2025-11-30 21:30:58 +0700

    Update api.js

--- js/services/api.js
+++ js/services/api.js
@@ -1,7 +1,7 @@
 const APIService = {
   async fetchData() {
     const resp = await fetch('data/dataBahanAjar.json');
-    if (!resp.ok) throw new Error('Gagal load data');
+    if (!resp.ok) throw new Error('Failed to load the data');
     return resp.json();
   }
 };
